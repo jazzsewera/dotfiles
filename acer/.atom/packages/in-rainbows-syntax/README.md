67b0d71f4231ba12480c82d07c1f57c266d98258
Blazej Sewera <blazejok1@wp.pl>
2018-12-28 00:13:21 +0100

    Removed unnecessary files

--- acer/.atom/packages/in-rainbows-syntax/README.md
+++
@@ -1,5 +0,0 @@
-# In Rainbows Syntax Theme for Atom editor
-
-Inspired in Radiohead's "In Rainbows" album. Feel free to fork it and modify it.
-
-![A screenshot of your theme](https://i.imgur.com/h9sAITj.jpg)
--- acer/.atom/packages/markdown-preview-plus/src/util-common.ts
+++
@@ -1,5 +0,0 @@
-export function getMedia(document: HTMLDocument) {
-  return document.querySelectorAll(
-    'img[src],audio[src],video[src]',
-  ) as NodeListOf<HTMLImageElement | HTMLAudioElement | HTMLVideoElement>
-}
