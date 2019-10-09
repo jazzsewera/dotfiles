20abfc50d5e269f3ed76411d2cb61077beca739b
Błażej Sewera <blazejok1@wp.pl>
2019-10-09 14:27:48 +0200

    Updated i3 config multihead and lock

--- acer/.config/i3/monitor.sh
+++ acer/.config/i3/monitor.sh
@@ -2,11 +2,11 @@
 
 xrandr \
   --output eDP1 --off \
-  --output HDMI1 --mode 1600x900 --rotate left --pos 0x0 \
-  --output DP2-1 --mode 1920x1200 --rotate normal --pos 900x400 --primary \
-  --output DP2-2 --mode 1920x1200 --rotate normal --pos 2820x400
+  --output HDMI1 --mode 1600x900 --rotate left --pos 0x0 --dpi 94 \
+  --output DP2-1 --mode 1920x1200 --rotate normal --pos 900x400 --primary --dpi 94 \
+  --output DP2-2 --mode 1920x1200 --rotate normal --pos 2820x400 --dpi 94
 
-sleep 0.3
+sleep 0.5
 
 wallschctl change &
 
