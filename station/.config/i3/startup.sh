f78f378670165d1595f7e07cc49ab14c7f57c34d
Błażej Sewera <blazejok1@wp.pl>
2020-01-10 20:46:02 +0100

    Added tearfree option in i3

--- station/.config/i3/startup.sh
+++ station/.config/i3/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 xrandr \
-  --output DVI-D-1 --auto --rotate left --pos 0x0 \
-  --output DisplayPort-0 --auto --rotate normal --pos 900x400 --primary \
-  --output DVI-D-0 --auto --rotate normal --pos 2820x400
+  --output DVI-D-1 --auto --rotate left --pos 0x0 --set TearFree on \
+  --output DisplayPort-0 --auto --rotate normal --pos 900x400 --primary --set TearFree on \
+  --output DVI-D-0 --auto --rotate normal --pos 2820x400 --set TearFree on
 
 wallschd &
 
